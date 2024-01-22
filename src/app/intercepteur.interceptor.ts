d94dd355c9aa66ccf3c287552a5ee09d15d1a47d
Zakpm <zakaryyapm@outlook.fr>
2024-01-22 13:17:18 +0100

    mise en place du seo

--- src/app/intercepteur.interceptor.ts
+++ src/app/intercepteur.interceptor.ts
@@ -2,26 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-// Déclaration de l'interface Authorization en dehors de la classe
-export interface Authorization {
-  type: string;
-  value: string;
-}
-
 @Injectable()
 export class IntercepteurInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-    const authToken = localStorage.getItem('token');
+    console.log('Intercepteur appelé');
+    const authToken = localStorage.getItem('jwtToken');
+    console.log('Token:', authToken);
 
     if (authToken) {
-    const authReq = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${authToken}`)
-    });
-    console.log('Request with Authorization header:', authReq); // Ajouter pour le débogage
-    return next.handle(authReq);
-  }
+      // Cloner la requête pour ajouter l'en-tête Authorization
+      // sans toucher à l'en-tête Content-Type existant
+      const authReq = req.clone({
+        setHeaders: { Authorization: `Bearer ${authToken}` }
+      });
+      console.log('Requête modifiée:', authReq);
+      return next.handle(authReq);
+    }
 
-  return next.handle(req);
-}
+    return next.handle(req);
+  }
 }
